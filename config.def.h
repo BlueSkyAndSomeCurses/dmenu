2573fbd093e2b2dd21c4cf0ce36296789568bfb6
Wouch <idontcare@noreply.xyz>
2023-09-14 13:25:17 +0300

    now dmenu is transparent

--- config.def.h
+++ config.def.h
@@ -2,14 +2,14 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xcf; /* Amount of opacity. 0xff is opaque */
+static const unsigned int alpha = 0xff; /* Amount of opacity. 0xff is opaque */
 static int centered = 0;                /* -c option; centers dmenu on screen */
 static int min_width = 500;
 /* Size of the window border */
 static unsigned int border_width = 0;
 static unsigned int columns = 0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"sans:size=19"};
+static const char *fonts[] = {"sans:size=16"};
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 
